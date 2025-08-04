8868d308286e0a2791bd1124fe5a9f80b921c627
OMPRAKASH.A <116077830+Omprakash143om@users.noreply.github.com>
2025-08-04 22:55:51 +0530

    Delete Binarysearch.py

--- Binarysearch.py
+++
@@ -1,8 +0,0 @@
-def linearsearch(arr,targetval):
-  for i in range(len(arr)):
-    if arr[i]==targetval:
-      return f"The index value is {i}"
-  return -1
-arr=list(map(int,input("Enter the numbers with space: ").split()))
-targetval=int(input("Enter targetval:"))
-print(linearsearch(arr,targetval))
